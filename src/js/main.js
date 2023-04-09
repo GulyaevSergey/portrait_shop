46ae761029d8fe994a821dce9bf4c1cbdae9fcf8
GulyaevSergey <109235648+GulyaevSergey@users.noreply.github.com>
2023-04-09 22:38:17 +0700

    create slider

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,7 @@
-import { modal } from "./modules/index";
+import { modal, sliders } from "./modules/index";
 
 document.addEventListener("DOMContentLoaded", () => {
     modal();
+    sliders(".feedback-slider-item", "", ".main-prev-btn", ".main-next-btn");
+    sliders(".main-slider-item", "vertical")
 });
--- src/js/modules/index.js
+++ src/js/modules/index.js
@@ -1,2 +1,2 @@
 export { modal } from "./modal";
-
+export { sliders } from "./sliders";
