ebb4ed4b858a20c928ef8b4339223cd9ac9008fa
GulyaevSergey <109235648+GulyaevSergey@users.noreply.github.com>
2023-04-30 00:04:28 +0700

    create main scroll

--- src/js/main.js
+++ src/js/main.js
@@ -10,6 +10,7 @@ import {
     pictureSize,
     accordion,
     burger,
+    scrolling,
 } from "./modules";
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -32,4 +33,5 @@ document.addEventListener("DOMContentLoaded", () => {
     pictureSize(".sizes-block");
     accordion(".accordion-heading", ".accordion-block");
     burger(".burger-menu", ".burger");
+    scrolling(".pageup");
 });
